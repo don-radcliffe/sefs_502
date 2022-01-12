60b75df5c808c783ae34d715a5778d987beacfe9
Don Radcliffe <donaldradcliffe91@gmail.com>
2022-01-12 09:08:29 -0800

    script to follow along with class two

--- scripts/class_two.R
+++ scripts/class_two.R
@@ -7,3 +7,13 @@ Oak_species <- read.csv("data/Oak_species_189x5.csv",
 Oak_explan <- Oak[ , ! colnames(Oak) %in%
                      Oak_species$SpeciesCode]
 head(Oak_explan)
+
+install.packages('labdsv')
+require('labdsv')
+labdsv::vegtab()
+
+vegtab(comm, set, minval = 1, pltord, spcord, pltlbl,
+       trans = FALSE)
+
+Oak_no_raw <- vegtab(comm = Oak_abund, minval = 0.05 * nrow(Oak_abund))
+View(Oak_no_raw)
