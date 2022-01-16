0b00127759af6107064bbab4bdbdad5a5b0ddd02
Don Radcliffe <donaldradcliffe91@gmail.com>
2022-01-16 10:34:00 -0800

    lecture four code, distance matrices

---
+++ scripts/class_four.R
@@ -0,0 +1,12 @@
+require(vegdist)
+
+test <- read.table("data/Legendre.Legendre.2012.p311.txt",
+                   header = TRUE)
+#To see the Euclidean distances among plots in test:
+  (ED.test <- dist(test))
+#Note that the result is displayed as a lower triangular matrix. What class is this object? It can
+#easily be converted to a full matrix:
+  as.matrix(ED.test)
+  
+dist(test)
+dist(test, method = 'manhattan')
