ca065839259e6b2422a4b7ca7fe96e64e0509696
Don Radcliffe <donaldradcliffe91@gmail.com>
2022-01-20 12:00:04 -0800

    Finished script homework two

--- scripts/assignment_two.R
+++ scripts/assignment_two.R
@@ -1,6 +1,13 @@
+## Don Radcliffe
+## Script to support assignment two, SEFS 502
+## Jon Bakker
+## January 19th, 2022
+
 vector <- c(1,7)
 V <- matrix(c(1,7)) #capital 'V'
 M <- matrix(c(5,10,2,1,7,8), nrow=2)
+
+##### Question 1 #####
 
 t(V) %*% M
 
@@ -24,4 +31,18 @@ V%*%vector
 
 vector%*%V
 
-dist(M)
+###### Question Two ######
+
+dist(M, method = 'euc')
+
+## Why are decostand results different than calculating manually?  I went with the manual calculation. 
+M_relativized_by_max <- M/max(M)
+M_relativized_by_max
+decostand(M, method = 'max')
+
+dist(M_relativized_by_max, method = 'euc')
+dist(M, method = 'euc')
+
+##### Question Three ######
+vegdist(M, method = 'bray')
+vegdist(M_relativized_by_max, method = 'bray')
